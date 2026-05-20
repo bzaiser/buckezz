4e32ef2c7fc30441d6d25c7d8ae9ded53876aa9a
Bernd Zaiser <bernd@zaisers.de>
2026-05-20 12:17:27 +0200

    feat: Add EMAIL_USE_SSL setting and update email env documentation

--- buckezz_project/settings.py
+++ buckezz_project/settings.py
@@ -177,10 +177,11 @@ PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'de-DE'
 
 # Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = env('EMAIL_BACKEND', default=env('MAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend'))
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='Buckezz <noreply@buckezz.app>')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default=env('EMAIL_HOST_USER', default='Buckezz <noreply@buckezz.app>'))
