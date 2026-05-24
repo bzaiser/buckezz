3ddaa2c170ef7fe6f35839fb46670cc0a7979553
Bernd Zaiser <bernd@zaisers.de>
2026-05-24 17:09:35 +0200

    feat: redirect authenticated users directly to the dashboard from landing page and login page

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ app_name = 'core'
 
 urlpatterns = [
     # Auth
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='core:home'), name='logout'),
     path('register/', views.RegisterTenantView.as_view(), name='register'),
     
--- core/views.py
+++ core/views.py
@@ -9,6 +9,11 @@ from .models import BucketList, ListItem, ListCategory, UserSetting, Person, Ite
 class HomeView(TemplateView):
     template_name = 'core/home.html'
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:dashboard')
+        return super().get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
